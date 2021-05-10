e70398a0dd07eccf7599b7c315b5e8390677c4cf
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-05-10 11:57:41 +0200

    Update odoo_payslip.rs

--- accounting/odoo_payslip.rs
+++ accounting/odoo_payslip.rs
@@ -1,1 +1,14 @@
-odoo_payslip.rs
+========================================
+Löneutbetalning. Köpa och bokföra mat i affäsrverksamheten för kunds räkning.
+========================================
+
+
+Planerade inköp
+------------
+
+
+Fakturera
+    Fakturera kunden för kommande inköp.
+
+
+.. image:: images/matkassan2_01.png
